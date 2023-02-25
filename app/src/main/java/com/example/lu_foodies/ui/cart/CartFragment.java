3a752d73f73ba3ce5f423e83989e43ecd362f9dd
MahtabAshfi <mahtabashfi1625@gmail.com>
2023-02-25 21:19:17 +0600

    checking

--- app/src/main/java/com/example/lu_foodies/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/lu_foodies/ui/cart/CartFragment.java
@@ -1,4 +1,4 @@
-package com.example.lu_foodies.ui.gallery;
+package com.example.lu_foodies.ui.cart;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,12 +10,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.lu_foodies.R;
 
+public class CartFragment extends Fragment {
 
-public class OrderFragment extends Fragment {
+    public CartFragment() {
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.order_fragment, container, false);
+        View root = inflater.inflate(R.layout.cart_fragment, container, false);
         return root;
     }
 }
